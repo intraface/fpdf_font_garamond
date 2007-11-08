5dda8be1a791eb6c1a9f62a10f96055878e9da56
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2007-11-08 13:08:10 +0000

    updated package and added garamond as an installable package

---
+++ 3Party/fpdf_font_garamond/font/font.php
@@ -0,0 +1,10 @@
+<?php
+require('../../include_intranet.php');
+require('fpdf/font/makefont/makefont.php');
+
+
+MakeFont("GARA.TTF", "gara.afm");
+MakeFont("GARABD.TTF", "garabd.afm");
+MakeFont("GARAIT.TTF", "garait.afm")
+
+?>
